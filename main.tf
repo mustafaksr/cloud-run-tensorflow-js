236ab9eb3ad4a9a4291c8ab4646f6b5fb9654681
mustafaksr <95983597+mustafaksr@users.noreply.github.com>
2023-01-23 16:43:32 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,14 +3,21 @@ provider "google" {
   region  = var.region
 }
 
-resource "google_cloud_run_service" "example" {
+
+resource "google_cloud_run_service" "default" {
   name     = "tf-js-regression"
   location = var.region
+  provider = google-beta
+  metadata {
+    annotations = {
+      "run.googleapis.com/launch-stage" = "BETA"
+    }
+  }
 
   template {
     spec {
       containers {
-        image = "mustafakeser/cloudruntensorflowjs:latest"
+        image = "gcr.io/civic-access-350318/cloudruntensorflowjs:latest"
         startup_probe {
             tcp_socket {
                 port = 3000
@@ -24,4 +31,20 @@ resource "google_cloud_run_service" "example" {
     percent         = 100
     latest_revision = true
   }
+}
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.default.location
+  project     = google_cloud_run_service.default.project
+  service     = google_cloud_run_service.default.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
 }
